7708fcfdd62a67c3388ab8967a822b2ffeaa49db
Abhishek55667 <ay698702@gmail.com>
2025-11-12 11:24:35 +0530

    done

--- src/main/java/com/Project/eCrimePortal/Entity/User.java
+++ src/main/java/com/Project/eCrimePortal/Entity/User.java
@@ -1,5 +1,6 @@
 package com.Project.eCrimePortal.Entity;
 
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -7,7 +8,6 @@ import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.time.LocalDate;
 
 @Entity
@@ -17,7 +17,6 @@ import java.time.LocalDate;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String password;
--- src/main/java/com/Project/eCrimePortal/Services/UserServices.java
+++ src/main/java/com/Project/eCrimePortal/Services/UserServices.java
@@ -5,6 +5,10 @@ import com.Project.eCrimePortal.Entity.User;
 import com.Project.eCrimePortal.Repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @Service
 public class UserServices {
@@ -14,4 +18,21 @@ public class UserServices {
     public void saveUser(User user){
         userRepo.save(user);
     }
+
+    public List<User> getAll(){
+        return userRepo.findAll();
+    }
+
+    public User getUser(int id){
+        return userRepo.findById(id).orElse(null);
+    }
+
+    public void deleteUser(int id){
+        userRepo.deleteById(id);
+    }
+
+    public void deleteAll(){
+        userRepo.deleteAll();
+    }
+
 }
